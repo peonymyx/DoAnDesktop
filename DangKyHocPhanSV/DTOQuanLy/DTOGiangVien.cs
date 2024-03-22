c63db3f93643b125643dcc64fa8936ebc9c350a5
peonymyx <peonymyx@gmail.com>
2024-03-23 02:55:07 +0700

    DoAn

---
+++ DangKyHocPhanSV/DTOQuanLy/DTOGiangVien.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOQuanLy
+{
+    public class DTOGiangVien
+    {
+        public string MaGV;
+        public string HoTen;
+        public string MaKhoa;
+        public DTOGiangVien() { }
+    }
+}
---
+++ DangKyHocPhanSV/DTOQuanLy/DTOMonHoc_DaoTao.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOQuanLy
+{
+    public class DTOMonHoc_DaoTao
+    {
+        public string MaMHDT;
+        public string MaMH;
+        public string MaCTDT;
+        public string MaNganh;
+        public DTOMonHoc_DaoTao()
+        {
+        }
+    }
+}
